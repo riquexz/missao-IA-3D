1da13c3d9e8f3beddee4f30a46d3b97c6ead8442
riquexz <161594551+riquexz@users.noreply.github.com>
2024-09-11 08:42:52 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,12 @@
 import {aleatorio}  from './aleatorio.js';
 import {perguntas} from './perguntas.js';
 
-
 const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
-
-
+const botaoJogarNovamente = document.querySelector(".novamente-btn");
 
 let atual = 0; 
 let perguntaAtual;
@@ -45,8 +43,15 @@ function mostraResultado(){
     caixaPerguntas.textContent = "Em 2049...";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = ""; 
+    caixaResultado.classlist.add("mostrar");
+    botaoJogarNovamente.addEventListener("click", jogarNovamente);
 }
 
-function aleatorio(){}
+function jogarNovamente {
+    atual = 0;
+    historiaFinal = "";
+    caixaResultado.classlist.remove("mostrar");
+    mostraPergunta();
+}
 
 mostraPergunta();
